e6705d2aa450bc3fa4a67505eb91689f6320f255
taiki-ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2020-01-03 16:42:26 +0900

    [change] work

--- work/bipedal/lipm_phase_portrait.py
+++ work/bipedal/lipm_phase_portrait.py
@@ -6,36 +6,14 @@ import matplotlib.animation as animation
 from scipy.integrate import odeint
 import scipy.spatial as ssp
 
+import sys
+sys.path.append('../../scripts/')
+from bipedal_robot.lipm import LinearInvertedPendulum
 
 def point_in_hull(point, hull, tolerance=1e-6):
   return all(
     (np.dot(eq[:-1], point) + eq[-1] <= tolerance)
     for eq in hull.equations)
-
-
-class LinearInvertedPendulum(object):
-    gravity = 9.8 # gravity
-
-    def __init__(self, height  = 1.5, weight = 50):
-        self.omega2 = self.gravity / height
-        self.omega = np.sqrt(self.omega2)
-
-        self.A = np.array([[0, 1],[self.omega2, 0]]) # state matrix
-        self.B = np.array([[0],[-self.omega2]]) # input matrix
-
-    def dX(self, X, t = 0):
-        if X.shape == np.zeros((self.A[0].size, 1)).shape:# normal
-            return self.A @ X
-        elif  X.ndim == 1: # for odeint
-            return self.A @ X.T
-        else: # for meshgrid
-            x_dot = np.zeros((X[:,0,0].size,X[0,0,:].size, X[0,:,0].size), dtype=np.float64)
-            for i in range(X[:,0,0].size):
-                x_tmp =  np.empty((0,X[0,0,:].size, X[0,:,0].size), dtype=np.float64)
-                for j in range(X[:,0,0].size):
-                    x_tmp = np.append(x_tmp, np.array([self.A[j,i] * X[i]]), axis = 0)    
-                x_dot += x_tmp
-            return x_dot
 
 if __name__ == '__main__':
     x_ref = np.array([[0.5],[0.]])
@@ -53,7 +31,7 @@ if __name__ == '__main__':
     x_d_min = -limit
     X1, X2 = np.meshgrid(np.arange(x_min, x_max, 0.5), np.arange(x_d_min, x_d_max, 0.5))
     X = np.array([X1, X2])
-    dX = plant.dX(X)
+    dX = plant.calc_dX(X)
 
     #normalize
     dX0_nr = dX[0] / (np.sqrt(dX[0]**2 + dX[1]**2) + 1e-6)
@@ -73,7 +51,7 @@ if __name__ == '__main__':
     for xi in np.linspace(x_min, x_max, step):
         for xj in np.linspace(x_d_min, x_d_max, step):
             if xj == x_d_min or xj == x_d_max:
-                x_data = odeint(plant.dX, [xi, xj], time)
+                x_data = odeint(plant.calc_dX, [xi, xj], time)
                 plt.plot(x_data[:,0], x_data[:,1], color='k', linewidth = 0.6)
                 # if x_data[-1,0] < 0.1 or x_data[-1,1] < 0.1:
                 #     point_in_hull([xi, xj], hull)
