6089ddd2cd8a3fe23a3352ee12806a84dda3ca46
taiki-ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2020-01-17 16:38:19 +0900

    [update] file structure

--- bipedal_robot/lipm/lipm_phase_portrait.py
+++ bipedal_robot/lipm/lipm_phase_portrait.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+from scipy.integrate import odeint
+import scipy.spatial as ssp
+
 class LinearInvertedPendulum(object):
     gravity = 9.8 # gravity
 
@@ -53,3 +56,58 @@ class LinearInvertedPendulum(object):
                     x_tmp = np.append(x_tmp, np.array([self.A[j,i] * X[i]]), axis = 0)    
                 x_dot += x_tmp
             return x_dot
+
+def point_in_hull(point, hull, tolerance=1e-6):
+  return all(
+    (np.dot(eq[:-1], point) + eq[-1] <= tolerance)
+    for eq in hull.equations)
+
+if __name__ == '__main__':
+    x_ref = np.array([[0.5],[0.]])
+    u_ref = np.array([[0.5]])
+    x_start = np.array([[0.],[0.]])
+    dt = 0.01
+    height = 1.5
+    plant = LinearInvertedPendulum(height)
+
+    #mesh
+    limit = 6
+    x_max = limit
+    x_min = -limit
+    x_d_max = limit
+    x_d_min = -limit
+    X1, X2 = np.meshgrid(np.arange(x_min, x_max, 0.5), np.arange(x_d_min, x_d_max, 0.5))
+    X = np.array([X1, X2])
+    dX = plant.calc_dX(X)
+
+    #normalize
+    dX0_nr = dX[0] / (np.sqrt(dX[0]**2 + dX[1]**2) + 1e-6)
+    dX1_nr = dX[1] / (np.sqrt(dX[0]**2 + dX[1]**2) + 1e-6)
+
+    #plot vetor
+    plt.quiver(X[0], X[1], dX0_nr, dX1_nr, color='b')
+
+    # xv = [1, -1,  1, -1]
+    # yv = [1,  1, -1, -1]
+    # bases = [xv, yv]
+    # hull = ssp.ConvexHull(bases)
+
+    #ode
+    time = np.arange(0, 2, 0.01)
+    step = 40 
+    for xi in np.linspace(x_min, x_max, step):
+        for xj in np.linspace(x_d_min, x_d_max, step):
+            if xj == x_d_min or xj == x_d_max:
+                x_data = odeint(plant.calc_dX, [xi, xj], time)
+                plt.plot(x_data[:,0], x_data[:,1], color='k', linewidth = 0.6)
+                # if x_data[-1,0] < 0.1 or x_data[-1,1] < 0.1:
+                #     point_in_hull([xi, xj], hull)
+
+    #counter
+    # plt.contour(X[0], X[1], dX[1], levels=[0], colors="red")
+    # plt.contour(X[0], X[1], dX[0], levels=[0], colors="Blue")
+
+    plt.xlim([x_min, x_max])
+    plt.ylim([x_d_min, x_d_max])
+    plt.grid()
+    plt.show()
--- bipedal_robot/lipm/lipm_simulate.py
+++ bipedal_robot/lipm/lipm_simulate.py
@@ -4,9 +4,42 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib.patches as patches
 
-import sys
-sys.path.append('../../scripts/')
-from bipedal_robot.lipm import LinearInvertedPendulum
+class LinearInvertedPendulum(object):
+    gravity = 9.8 # gravity
+
+    def __init__(self, height  = 1.5, weight = 50):
+        self.height = height
+        self.omega2 = self.gravity / self.height
+        self.omega = np.sqrt(self.omega2)
+
+        self.A = np.array([[0, 1],[self.omega2, 0]]) # state matrix
+        self.B = np.array([[0],[-self.omega2]]) # input matrix
+
+    
+    def init_state(self, x, x_ref, u_ref, dt = 0.01, x_dot = 0., x_ddot = 0):
+        self.X = np.array([[x], [x_dot]])
+        self.X_dot = np.array([[x_dot], [x_ddot]])
+        self.u = self.bestCOG_Regulator(x_ref, u_ref)
+        self.dT = dt
+
+    def do_action(self, x_ref, u_ref):
+        self.u = self.bestCOG_Regulator(x_ref, u_ref)
+        self.update_state()
+
+    def update_state(self):
+        self.X_dot = self.A @ self.X + self.B @ self.u
+        self.X =self.X + self.X_dot * self.dT 
+
+    def bestCOG_Regulator(self, x_ref, u_ref):
+        self.alpha = 3.0
+        self.F = self.alpha * np.array([[1.0, 1.0/self.omega2]])
+        return u_ref - self.F @ (x_ref - self.X)
+
+    def get_X(self):
+        return self.X
+    
+    def get_u(self):
+        return self.u
 
 def video(x_hs, u_hs, h, t, fig):
     ax = fig.add_subplot(221, aspect='equal', autoscale_on=False, xlim=(-2, 2), ylim=(-0.5, 3.5))
